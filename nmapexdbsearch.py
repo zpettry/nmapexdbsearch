ba7ab9a6633735bad43b2d07f206edbcc8b6b552
zpettry <zpettry@protonmail.com>
2017-05-11 11:53:09 -0500

    Update nmapexdbsearch.py
    Added comments and input operations. General cleanup.

--- nmapexdbsearch.py
+++ nmapexdbsearch.py
@@ -4,8 +4,9 @@ import subprocess
 import xml.etree.ElementTree as ET
 
 def acquire_input_and_run_nmap():
-	host = sys.argv[1]
-	command = ("nmap -oX - -A -Pn %s" % host)
+	host = input("What is the hostname or IP address?  ")
+	ports = input("What port(s) do you want to scan? ex. 80 or 1-65535  ")
+	command = ("nmap -oX - -A -Pn %s -p %s" % (host, ports))
 	p = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
 	root = ET.fromstring(p.stdout)
 	return root
@@ -15,19 +16,25 @@ def process_nmap_port_services(root):
 	for service in root.iter('service'):
 		output = service.get('product')
 		info.append(output)
-	info = [x for x in info if x != None]
-	print(info)
+	return info
+
+def clean_output(info):
+	info = [x for x in info if x != None] # Takes out None types
+	print("\nThese services have been found %s\n" % info)
+	info = ([s.strip('httpd') for s in info]) # Takes out httpd because it limits search results
 	return info
 
 def searchsploit_search(cleanservices):
 	for cleanservices in cleanservices:
+		print("\nSEARCHING FOR THE %s SERVICE\n" % cleanservices)
 		command = ("searchsploit %s" % cleanservices)
 		subprocess.run(command, shell=True)
 		
 def main():
+	print("\nMake sure the searchsploit program is up-to-date. i.e. searchsploit -u \n")
 	root = acquire_input_and_run_nmap()
 	services = process_nmap_port_services(root)
-	cleanservices = [x for x in services if x != None]
+	cleanservices = clean_output(services)
 	searchsploit_search(cleanservices)
 	
 if __name__ == "__main__":
